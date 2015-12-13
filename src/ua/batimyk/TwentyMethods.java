0b4e0fba02aee7d33d747937bb780e06046ee24e
nbatyts <nbatyts@gmail.com>
2015-12-13 22:01:25 +0200

    added junit file for TwentyMethods

--- src/ua/batimyk/TwentyMethods.java
+++ src/ua/batimyk/TwentyMethods.java
@@ -29,7 +29,7 @@ public class TwentyMethods {
     public static void invert(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (-1) * array[i][j];
+                array[i][j] *= -1;
             }
         }
     }
@@ -54,28 +54,20 @@ public class TwentyMethods {
         return s;
     }
 
-
     public static boolean include(char[] array, char[] arrayToFind) {
 
-        for (int i = 0, j = 0; i < array.length; i++) {
-
-            if (j == arrayToFind.length ) {
-                return true;
-            }
+        for (int i = 0, j = 0; i < array.length; i++, j++) {
 
             if (array[i] != arrayToFind[j]) {
-                j = 0;
-            } else {
-                j++ ;
+                i -= j;
+                j = -1;
             }
-            System.out.println(i + " " + j + " " + array[i] + " " + arrayToFind[j] );
-
-
+            if (j == arrayToFind.length - 1) return true;
         }
         return false;
     }
 
-    public static int index(int[] array, int value) {
+    public static int indexOf(int[] array, int value) {
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] == value) {
@@ -85,9 +77,9 @@ public class TwentyMethods {
         return -1;
     }
 
-    public static int indexReversal(int[] array, int value) {
+    public static int indexOfReversal(int[] array, int value) {
 
-        return index(array, value) == -1 ? - 1 :  array.length - index(array, value) - 1 ;
+        return indexOf(array, value) == -1 ? -1 : array.length - indexOf(array, value) - 1;
     }
 
     public static int factorial(int value) {
@@ -103,7 +95,7 @@ public class TwentyMethods {
     }
 
     public static String[] getIncluded(String[] array, String value) {
-        //?not sure is it the best solution 		
+
         int n = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[n].contains(value)) {
@@ -170,7 +162,7 @@ public class TwentyMethods {
     }
 
     public static boolean isHasDupValue(byte[] ab) {
-    //two arrays
+        //two arrays
         for (int i = 0; i < ab.length; i++) {
             for (int j = i + 1; j < ab.length; j++) {
                 if (ab[i] == ab[j]) {
@@ -180,72 +172,4 @@ public class TwentyMethods {
         }
         return false;
     }
-
-    public static void main(String[] args) {
-
-        char[] array = {'a', 'b', 'c', 'd','e' ,'e', 'f','f'};
-        char[] array2 = {'e', 'f'};
-        char[] array3 = {'b', 'd'};
-        char[] array4 = {'f'};
-        char[] array5 = {'a', 'b'};
-        char[] array6 = {'a', 'b', 'd'};
-
-        String[] strArr = {"abc", "def", "ghj", "rty", "sdfabcgh", "swe"};
-
-        int[] iarray = {1, 4, 1001, 6, 78, -86, 887, 33, 1000, 0, 5};
-        byte[] barray = {1, 5, 7, 8, 10, 35, 2, 4, 9, 0};
-        byte[] barray2 = {1, 5, 7, 8, 10, 35, 2, 4, 9, 0, 0};
-
-        int[][] arrayInt = {{65, 66, 67}, {67, 68}, {69, 66, 70}};
-
-
-        System.out.println(include(array,array2));
-      //  System.out.println(include(array,array));
-      //  System.out.println(include(array,array5));
-     //   System.out.println(include(array,array4));
-     //   System.out.println(include(array,array3));
-
-        //System.out.println(isHasDupValue(barray));
-        //System.out.println(isHasDupValue(barray2));
-
-        /*for (char[] x : to2dCharArray(arrayInt)) {	  
-         for( char y : x ) System.out.print(y+" ");
-         System.out.println("");
-         }*/
-	//printDouble3digit(1.324355d);
-        //printDouble3digit(12222.324355d);
-        //printDouble3digit(12222.32d);
-        //sortDescending(iarray);
-      //  sortConditional(iarray, true);
-      //  for (int i : iarray) {
-      //      System.out.println(i);
-      //  }
-
-	//printMultiples(iarray, 2);
-        //for (String s : getIncluded(strArr,"bc")) System.out.println("return: "+s);
-        //System.out.println(getFactorial(6));
-        //System.out.println(getFactorial(5));
-        //System.out.println(getFactorial(1));
-        //System.out.println(getFactorial(0));
-        //System.out.println(getFactorial(10));
-	//System.out.println(isInclude(array,array ));
-        //System.out.println(isInclude(array,array2 ));
-        //System.out.println(isInclude(array,array3 ));
-        //System.out.println(isInclude(array,array4 ));
-        //System.out.println(isInclude(array,array5 ));
-        //System.out.println(isInclude(array,array6 ));
-        //System.out.println(isInclude(array6,array5 ));
-	//System.out.println(isLeapYear(2000));
-        //System.out.println(isLeapYear(1600));
-        //System.out.println(isLeapYear(2002));
-	//System.out.println(getIndex(iarray, 6));
-        //System.out.println(getIndex(iarray, 5));		
-        //System.out.println(getIndex(iarray, -86));	
-        //System.out.println(getIndex(iarray, 2));	
-	//System.out.println(getIndexFromEnd(iarray, 6));
-        //System.out.println(getIndexFromEnd(iarray, 5));		
-        //System.out.println(getIndexFromEnd(iarray, -86));	
-        //System.out.println(getIndexFromEnd(iarray, 2));	
-    }
-
 }
