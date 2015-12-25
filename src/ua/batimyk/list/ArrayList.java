d91600a742adc8104ec3094091334ba5e5479ea6
nb <nbatyts@gmail.com>
2015-12-25 17:47:35 +0200

    changes in ArrayList class

--- src/ua/batimyk/list/ArrayList.java
+++ src/ua/batimyk/list/ArrayList.java
@@ -7,62 +7,83 @@ public class ArrayList implements List {
 
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
-    Object[] element;
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    private static final float INCRFEASING_COEFFICIENT = 1.5f;
 
-    void add(Object value) {
-        element[size++] = value;
+    protected int capacity = 0;
+
+    Object[] elements;
+
+    public void add(Object value) {
+        elements[size++] = value;
     }
 
-    void add(int index, Object value){
-        rangeCheck(index);
+    public void add(int index, Object value){
+        rangeCheckAdd(index);
     }
 
-    Object set(int index, Object value){
+    public Object set(int index, Object value){
         rangeCheck(index);
-        Object elementOld = element[index];
-        element[index] = value;
+        Object elementOld = elements[index];
+        elements[index] = value;
         return elementOld;
     }
 
-    Object remove(int index){
+    public Object remove(int index){
         rangeCheck(index);
-        Object elementOld = element[index];
+        Object elementOld = elements[index];
         return elementOld;
     }
 
-    int size(){
+    public int size(){
        return size;
     }
 
-    boolean isEmpty(){
+    public boolean isEmpty(){
        return size == 0;
     }
 
-    void clear(){
+    public void clear(){
 
     }
 
-    Object get(int index){
+    public Object get(int index){
         rangeCheck(index);
-        return element[index];
+        return elements[index];
     }
 
-    int indexOf(Object value){
+    public int indexOf(Object value){
         return -1;
     }
 
-    int lastIndexOf(Object value){
+    public int lastIndexOf(Object value){
         return -1;
     }
 
-    boolean contains(Object value){
+    public boolean contains(Object value){
         return false;
     }
 
     private void rangeCheck(int index){
-        if(index >= size) {
+        if(index > size ) {
             throw new IndexOutOfBoundsException("Index: "+ index +", Size: "+ size);
         }
     }
 
+    private void rangeCheckAdd(int index){
+        if(index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: "+ index +", Size: "+ size);
+        }
+    }
+
+    private void increase(int capacity) {
+        if(capacity > this.capacity)
+        {
+            this.capacity *= INCRFEASING_COEFFICIENT;
+        }
+    }
+
+    private void checkCapacity(int capacity) {
+
+    }
 }
