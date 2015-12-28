58ea65cfc09f87e1e4891084254f090813933b3b
Aleksandr Myftakhov <aleksandr.myftakhov@db.com>
2015-12-28 12:20:32 +0200

    arrays tests

---
+++ test/ua/com/omiftakhov/arraysManipulationAndCo/LinkedListTest.java
@@ -0,0 +1,14 @@
+package ua.com.omiftakhov.arraysManipulationAndCo;
+
+import org.junit.Before;
+/**
+ * Created by omiftakhov on 28.12.2015.
+ */
+public class LinkedListTest extends ListTest {
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        list= new LinkedList();
+
+    }
+}
